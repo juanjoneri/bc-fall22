0565e45526622def92a9ce1576cf83ab72706acb
Juan José <juanj@DESKTOP-47SH0M2.localdomain>
2022-11-24 13:09:39 -0600

    add ability to sign up

--- nyt/lightning/urls.py
+++ nyt/lightning/urls.py
@@ -1,8 +1,8 @@
 from django.urls import path
-
 from . import views
 
 urlpatterns = [
     path('articles/', views.index, name='index'),
     path('articles/<int:article_id>', views.article, name='article'),
+    path('signup/', views.signup, name='signup'),
 ]
--- nyt/lightning/views.py
+++ nyt/lightning/views.py
@@ -1,20 +1,32 @@
 from django.http import HttpResponse
 from django.template import loader
-
+from django.contrib.auth.decorators import login_required
 from .models import Article
+from django.contrib.auth import login, authenticate
+from django.contrib.auth.forms import UserCreationForm
+from django.shortcuts import render, redirect
+
+def signup(request):
+    if request.method == 'POST':
+        form = UserCreationForm(request.POST)
+        if form.is_valid():
+            form.save()
+            username = form.cleaned_data.get('username')
+            raw_password = form.cleaned_data.get('password1')
+            user = authenticate(username=username, password=raw_password)
+            login(request, user)
+            return redirect('index')
+    else:
+        form = UserCreationForm()
+    return render(request, 'lightning/signup.html', {'form': form})
 
 def index(request):
     articles = Article.objects.values
-    template = loader.get_template('lightning/index.html')
-    context = {
-        'articles': articles,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'articles': articles}
+    return render(request, 'lightning/index.html', context)
 
+@login_required
 def article(request, article_id):
     article = Article.objects.get(id=article_id)
-    template = loader.get_template('lightning/article.html')
-    context = {
-        'article': article,
-    }
-    return HttpResponse(template.render(context, request))
+    context = {'article': article, 'user': request.user}
+    return render(request, 'lightning/article.html', context)
